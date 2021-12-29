41d0b7d21b7a7815175aa3e521f44fa398f3b965
Tomás Catena <tomascatena@gmail.com>
2021-12-29 14:12:50 +0100

    add Header

--- packages/container/src/App.tsx
+++ packages/container/src/App.tsx
@@ -1,15 +1,15 @@
 import React, { FC } from 'react';
+import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App: FC = () => {
   return (
-    <div>
-      <h1>Container App</h1>
-
-      <hr />
+    <BrowserRouter>
+      <Header signedIn={false} onSignOut={() => {}} />
 
       <MarketingApp />
-    </div>
+    </BrowserRouter>
   );
 };
 
